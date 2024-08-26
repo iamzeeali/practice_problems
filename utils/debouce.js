a88da0a63813c6465a6fa9a7b9fe7567bcaa130d
Md Zeeshan Ali <Mdzeeshanali93@gmail.com>
2024-08-26 12:04:42 +0530

    debouce added

---
+++ utils/debouce.js
@@ -0,0 +1,10 @@
+let debounce = function (func, wait) {
+    let id;
+
+    return function (...args) {
+        clearTimeout(id)
+        id = setTimeout(() => func(...args), wait)
+    }
+}
+
+const log = debounce(console.log, 500)
