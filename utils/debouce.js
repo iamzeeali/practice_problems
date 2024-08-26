d256767e5fcc531d23bca9845be79a6d0cd55b68
Md Zeeshan Ali <Mdzeeshanali93@gmail.com>
2024-08-26 12:09:26 +0530

    debouce

--- utils/debouce.js
+++ utils/debouce.js
@@ -1,6 +1,11 @@
+// debouncing is a technique used to limit the rate at which a function is executed. 
+// It’s especially useful for scenarios where a function might be triggered frequently, 
+// such as handling user input events like scrolling, resizing, or typing.
+
 let debounce = function (func, wait) {
     let id;
     return function (...args) {
+        // cancels the second,thirs.. calls if one function call is not completed. 
         clearTimeout(id);
         id = setTimeout(() => func(...args), wait);
     };
