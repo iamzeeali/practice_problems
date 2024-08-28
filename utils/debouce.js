d8fb2a2289146cb074d77399b6cf2e3344de0736
Md Zeeshan Ali <Mdzeeshanali93@gmail.com>
2024-08-28 23:38:42 +0530

    debounce updated

--- utils/debouce.js
+++ utils/debouce.js
@@ -2,14 +2,31 @@
 // It’s especially useful for scenarios where a function might be triggered frequently, 
 // such as handling user input events like scrolling, resizing, or typing.
 
+// ------------------------------------------------------------------------------------------------
 let debounce = function (fn, wait) {
-    let id;
+    let id; // Variable to store the timeout ID
+
     return function (...args) {
-        // cancels the second,third, fourth, etc function calls if one function call is not completed. 
+        // Clear the previous timeout if a new call is made before the wait period elapses
         clearTimeout(id);
+
+        // Set a new timeout to call the function after the specified wait period
         id = setTimeout(() => fn(...args), wait);
     };
 };
+// ------------------------------------------------------------------------------------------------
 
-const log = debounce(console.log('hello'), 50000);
+// Function to debounce
+function logMessage(message) {
+    console.log(message);
+}
 
+// Create a debounced version of logMessage
+const debouncedLog = debounce(logMessage, 1000);
+
+// Call the debounced function multiple times
+debouncedLog('Hello');
+debouncedLog('Hello, World!');
+debouncedLog('Hello, Debounce!');
+
+// Only "Hello, Debounce!" will be logged after 1 second
